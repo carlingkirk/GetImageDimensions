fe324e8c82a07136e09c281c3d7064ef0419e0fb
carlingkirk <carling@degreed.com>
2018-04-24 11:59:38 -0700

    Initial commit

---
+++ ImageSizes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageSizes
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Enter Image URL:");
+            var url = Console.ReadLine();
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    var image = Image.FromStream(stream);
+
+                    (float Width, float Height) dimensions = (image.PhysicalDimension.Width, image.PhysicalDimension.Height);
+
+                    Console.WriteLine($"Width: {dimensions.Width}, Height: {dimensions.Height}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
